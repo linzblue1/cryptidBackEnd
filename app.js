a5e1c8f26dc18ca1f3430b8e45ea9a689c374154
Blue Lindsey <linzblue5@gmail.com>
2020-04-22 16:33:00 -0500

    Init commit

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var next = require('next')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'pages')));
 
-var app = express();
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.prepare()
+    .then(() => {
+        const server = express()
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+        server.get('*', (req, res) => {
+            return handle(req, res)
+        })
+
+        server.listen(3001, (err) => {
+            if (err) throw err
+            console.log('> Ready on http://localhost:3001')
+        })
+    })
+    .catch((ex) => {
+        console.error(ex.stack)
+        process.exit(1)
+    })
 
 module.exports = app;
