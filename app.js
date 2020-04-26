c00d165c7b7e296a90c3ee78352a38dd3990d0d2
bdcolosi <bdcolosi27@gmail.com>
2020-04-26 18:22:14 -0400

    added users model, bcrypt, now working to respond to front end

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const upload = require("express-fileupload");
 
-
-// const port = process.env.PORT || 3001;
+app.use(upload());
+console.log("Server Started!");
 
 app.use(logger("dev"));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(
+  session({
+    resave: false,
+    secret: "hello",
+    saveUninitialized: true,
+    is_logged_in: false,
+  })
+);
 
 const indexRouter = require("./routes/index");
 app.use("/", indexRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,62 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express"),
+  router = express.Router(),
+  bcrypt = require("bcrypt"),
+  usersModel = require("../models/users");
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.sendStatus(200);
+router.get("/login", function (req, res, next) {
+  res.render("template", {
+    locals: {
+      title: "Sign In",
+      is_logged_in: req.session.is_logged_in,
+    },
+    partials: {
+      partial: "partial-login",
+    },
+  });
+});
+
+router.post("/login", async function (req, res, next) {
+  const { email, password } = req.body;
+
+  const user = new usersModel(null, null, email, password);
+  const loginResponse = await user.userLogin();
+  // console.log('login response is', loginResponse);
+  if (!!loginResponse.isValid) {
+    req.session.is_logged_in = loginResponse.isValid;
+    req.session.user_id = loginResponse.id;
+    req.session.name = loginResponse.name;
+    res.redirect("/");
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.get("/signup", (req, res) => {
+  res.render("template", {
+    locals: {
+      title: "Sign Up",
+      is_logged_in: req.session.is_logged_in,
+    },
+    partials: {
+      partial: "partial-signup",
+    },
+  });
+});
+
+router.post("/signup", async (req, res) => {
+  const { name, email } = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
+  const user = new usersModel(null, name, email, username, hash);
+  user.save().then(() => {
+    res.redirect("/users/login");
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
 });
 
 module.exports = router;
