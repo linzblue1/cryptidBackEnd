bbc96762468b79702b7e926354cedc2443147455
Blue Lindsey <linzblue5@gmail.com>
2020-04-26 01:51:20 -0500

    Refactored for socket.io

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 
+
 // const port = process.env.PORT || 3001;
 
 app.use(logger('dev'));
@@ -11,8 +12,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const usersRouter = require("./routes/users");
-app.use("/users", usersRouter);
+app.get('/', function (req, res) {
+    res.send('<h1>hello world</h1>')
+});
 
 
 module.exports = app;
